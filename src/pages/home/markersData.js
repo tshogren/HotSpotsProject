e7eb1c12ab2d20444a00b5377223eb19efd4a79c
Michael <mmaldona@macalester.edu>
2018-10-17 10:28:54 -0500

    Included custom icons for academic buildings

--- src/pages/home/markersData.js
+++ src/pages/home/markersData.js
@@ -1,65 +1,83 @@
+let academicIcon = {
+  'url': 'assets/imgs/academicBuilding.png',
+  'anchor': [10, 20],
+  'size': {
+    width: 20,
+    height: 19
+  }
+};
+
 export const markersDataArray = [
   { name: 'Old Main',
     position: {
       lat: 44.93858,
       lng: -93.16854
     },
-    description: 'Home of Humanities'
+    description: 'Home of Humanities',
+    icon: academicIcon
   },
   { name: 'Leonard Center',
     position: {
       lat: 44.937286,
       lng: -93.16766843260166
     },
-    description: 'Athletics and Health and Wellness located here. Also has an additional dining option in Scottys'
+    description: 'Athletics and Health and Wellness located here. Also has an additional dining option in Scottys',
+    icon: 'red'
   },
   { name: 'Neill Hall',
     position: {
       lat: 44.93725802043779,
       lng: -93.1692529845086
     },
-    description: 'Home of most language programs at Macalester'
+    description: 'Home of most language programs at Macalester',
+    icon: academicIcon
   },
   { name: 'Olin-Rice Science Center ',
     position: {
       lat: 44.93666646373501,
       lng: -93.16894868324005
     },
-    description: 'Home of Math and Science'
+    description: 'Home of Math and Science',
+    icon: academicIcon
   },
   { name: 'Janet Wallace Fine Arts Center',
     position: {
       lat: 44.937578182963996,
       lng: -93.16967579323779
     },
-    description: 'Home of Fine Arts'
+    description: 'Home of Fine Arts',
+    icon: academicIcon
   },
   { name: 'Janet Wallace Fine Arts Center',
     position: {
       lat: 44.937578182963996,
       lng: -93.16967579323779
     },
-    description: 'Home of Fine Arts'
+    description: 'Home of Fine Arts',
+    icon: academicIcon
   },
   { name: 'DeWitt Wallace Library',
     position: {
       lat: 44.93845519992089,
       lng: -93.16818770313091
     },
-    description: 'Macalester\'s Library'
+    description: 'Macalester\'s Library',
+    icon: 'red'
   },
   { name: 'Ruth Stricker Dayton Campus Center',
     position: {
       lat: 44.93947081646128,
       lng: -93.16763728766716
     },
-    description: 'Home of a majority of Macalester\'s dining options, as well as mailing services'
+    description: 'Home of a majority of Macalester\'s dining options, as well as mailing services',
+    icon: 'red'
   },
   { name: 'Carnegie Hall',
     position: {
       lat: 44.93863853335998,
       lng: -93.16919008825039
     },
-    description: 'Home of Economics'
+    description: 'Home of Economics',
+    icon: academicIcon
   }
 ];
